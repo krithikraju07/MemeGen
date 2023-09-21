c51e82add60593cb01dfd77f0492003819eea433
krithikraju07 <97392551+krithikraju07@users.noreply.github.com>
2023-09-21 23:10:11 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header className="header">
+            <img src="../troll-face.png" className="header-image"/>
+            <h1 className="header-title">Meme Generator</h1>
+        </header>
+    )
+}
